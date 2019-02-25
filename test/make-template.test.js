e1f9735b4ac995cee34c4b4b7ffc8cf412abf4be
Cheri Portman <cheriportman@hotmail.com>
2019-02-25 15:32:17 -0800

    data hardcoded in html, testing framed out but not yet figured out

---
+++ test/make-template.test.js
@@ -0,0 +1,14 @@
+const test = QUnit.test;
+
+
+QUnit.module('template creation test');
+
+test('create a template to create image tiles', function(assert) {
+    //arrange
+
+    
+    //act
+    
+    //assert
+    assert.equal(result, expected);
+});
