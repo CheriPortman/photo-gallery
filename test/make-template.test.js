c6873810b9c4cf99b1f1b742214bb2993dbf621e
Cheri Portman <cheriportman@hotmail.com>
2019-02-25 17:24:23 -0800

    failing first test

--- test/make-template.test.js
+++ test/make-template.test.js
@@ -3,6 +3,15 @@ import images from '../data/images.js';
 
 const test = QUnit.test;
 
+function makeImageTemplate() {/*html*/`
+    <li>
+        <h2>UniWhal</h2>
+        <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+    </li>
+`;
+
+}
+
 
 QUnit.module('template creation test');
 
@@ -11,9 +20,14 @@ test('create a template to create image tiles', function(assert) {
     
 
     //act
-    
+    const expected = `
+        <li>
+            <h2>UniWhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+        </li>
+    `;
 
     //assert
-
+    assert.htmlEqual(result, expected);
 
 });
