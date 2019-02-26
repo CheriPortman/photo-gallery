207fe86ef2e63c8b852ec98ed9a064351216992d
Cheri Portman <cheriportman@hotmail.com>
2019-02-25 17:39:11 -0800

    test with hard-coded data passes

--- test/make-template.test.js
+++ test/make-template.test.js
@@ -1,26 +1,33 @@
-import images from '../data/images.js';
+// import images from '../data/images.js';
 // import makeImageTemplate from '../make-image-template.js';
 
 const test = QUnit.test;
 
-function makeImageTemplate() {/*html*/`
-    <li>
-        <h2>UniWhal</h2>
-        <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
-    </li>
-`;
+QUnit.module('template creation test');
+
+
+function makeImageTemplate() {
+    const html = /*html*/`
+        <li>
+            <h2>UniWhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+        </li>`;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    const dom = template.content;
+    return dom;
 
 }
 
 
-QUnit.module('template creation test');
 
 test('create a template to create image tiles', function(assert) {
     //arrange
-    
+    const result = makeImageTemplate();
 
     //act
-    const expected = `
+    const expected = /*html*/ `
         <li>
             <h2>UniWhal</h2>
             <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
